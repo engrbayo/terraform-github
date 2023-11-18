d98215440577ada0766b8fcaad7e747c19ffd168
engrbayo11 <aadedoyinadebayo2013@gmial.com>
2023-11-18 12:31:23 -0600

    new fix

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "second-sentrick"
     key    = "path/s3-bucket"
-    region = "us-east-1"
-    dynamodb_table = "my-lock-table"
+    region = "us-east-2"
+    dynamodb_table = "new_db_table"
   }
 }
