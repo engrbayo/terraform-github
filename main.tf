6d41f67e64010f4c4bd74c3be2e47b6fb0b976a2
Supersaver01 <128857553+Supersaver01@users.noreply.github.com>
2023-11-18 10:52:26 -0600

    new workflow added with resource to provision

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_default_vpc" "default" {
 }
 
 # Security group
-resource "aws_security_group" "demo_sg_terraform" {
-  name        = "demo_sg_terraform"
+resource "aws_security_group" "demo_sg_terraform-new" {
+  name        = "demo_sg_terraform-new"
   description = "allow ssh on 22 & http on port 80"
   vpc_id      = aws_default_vpc.default.id
 
@@ -35,11 +35,11 @@ resource "aws_instance" "aws_ubuntu" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.ubuntu.id
   user_data              = file("userdata.tpl")
-  vpc_security_group_ids = ["${aws_security_group.demo_sg_terraform.id}"]
+  vpc_security_group_ids = ["${aws_security_group.demo_sg_terraform-new.id}"]
   key_name               = "EC2Instance"
 
     tags = {
-    Name = "New_Git_EC2"
+    Name = "Pull_Git_EC2"
   }
 
 }
